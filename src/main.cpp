caf33291251be6b83db993fbae91efbd23291c57
DonPavlov <1541649+DonPavlov@users.noreply.github.com>
2023-08-12 17:08:59 +0200

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,11 @@
 
 #include <Adafruit_MCP23X17.h>
 
-
-#define ARCADE_N (18)
-#define ARCADE_S (16)
-#define ARCADE_W (17)
-#define ARCADE_E (15)
+// UART1 GPIO 18 RX GPIO17 TX
+#define ARCADE_N (16)
+#define ARCADE_S (15)
+#define ARCADE_W (14)
+#define ARCADE_E (8)
 
 unsigned long startMillis;         // some global variables available anywhere in
                                    // the program
@@ -28,6 +28,7 @@ byte    motorPort = (byte)DuploTrainHubPort::MOTOR;
 LightStrip myStrip; // Create an instance of the LightStrip class
 
 Adafruit_7segment matrix = Adafruit_7segment();
+Adafruit_MCP23X17 mcp;
 
 void colorSensorCallback(void      *hub,
                          byte       portNumber,
@@ -96,6 +97,10 @@ void speedometerSensorCallback(void      *hub,
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Set ESP32 pins 5 and 9 as INPUT_PULLUP
+  pinMode(5,           INPUT_PULLUP);
+  pinMode(9,           INPUT_PULLUP);
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage
                                    // level)
   matrix.begin(0x70);              // Init I2C Display
@@ -114,6 +119,28 @@ void setup()
   Serial.begin(115200);
   digitalWrite(LED_BUILTIN, LOW); // turn the LED off
   startMillis = millis();         // initial start time
+
+  // configure buttons
+  if (!mcp.begin_I2C())
+  {
+    // if (!mcp.begin_SPI(CS_PIN)) {
+    Serial.println("Error.");
+
+    while (1)
+      ;
+  }
+
+  // Configure Port A pins 0 to 5 as INPUT and enable pull-up resistors
+  for (int i = 0; i < 6; i++)
+  {
+    mcp.pinMode(i, INPUT_PULLUP);
+  }
+
+  // Configure Port B pins B5 to B0 as OUTPUT
+  for (int i = 13; i >= 8; i--)
+  {
+    mcp.pinMode(i, OUTPUT);
+  }
 }
 
 void loop()
@@ -131,8 +158,6 @@ void loop()
                                              // time of the current LED
                                              // state.
   }
-
-  Serial.println("Rainbow ");
 
   if (myHub.isConnecting())
   {
@@ -161,5 +186,36 @@ void loop()
     {
       Serial.println("Failed to connect to Duplo Hub");
     }
+  }
+  test_inputs();
+}
+
+void test_inputs()
+{
+  for (int i = 0; i < 6; i++)
+  {
+    int inputValue = mcp.digitalRead(i);
+    Serial.print("Input ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(inputValue);
+  }
+
+  // Read ESP32 pins 5 and 9 and print their values
+  int esp32Input5 = digitalRead(5);
+  int esp32Input9 = digitalRead(9);
+
+  Serial.print("ESP32 Pin 5: ");
+  Serial.println(esp32Input5);
+
+  Serial.print("ESP32 Pin 9: ");
+  Serial.println(esp32Input9);
+
+  // Enable and disable LEDs sequentially with a half-second delay
+  for (int i = 13; i >= 8; i--)
+  {
+    mcp.digitalWrite(i, HIGH); // Turn on LED
+    delay(500);                // Wait for 500 milliseconds
+    mcp.digitalWrite(i, LOW);  // Turn off LED
   }
 }
